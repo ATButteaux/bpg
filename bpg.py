5bdc8186a7e21cdf5a875cbcde4d0915f2fcf7f3
ATButteaux <113801661+ATButteaux@users.noreply.github.com>
2023-10-02 18:37:05 -0700

    Update bpg.py

--- bpg.py
+++ bpg.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+from ast import literal_eval
 import base58
 import hashlib
 import sys
 
 def showHelp():
+  print("bpg.py ver 20231002")
+  print()
   print("HELP")
   print("====")
   print("ca     Clear all")
@@ -11,6 +15,7 @@ def showHelp():
   print("s XYZ  Change secret to XYZ")
   print("dc     Data Clear")
   print("d XYZ  Change data to XYZ")
+  print("q      Quit and wipe working data")
   print("sd     Change secret to decimal string")
   print("sh     Change secret to sha256 hash")
   print("sl     Change secret to left side Base58")
@@ -27,11 +32,11 @@ encoding_string = ""
 hash_string = ""
 decimal_string = ""
 base58_string = ""
+hash = ""
 
 while True:
   print("Secret:       " + secret_string)
   print("Data:         " + data_string)
-  print("Encoding:     " + encoding_string)
   print("Hash:         " + hash_string)
   print("Decimal:      " + decimal_string)
   print("Base58:       " + base58_string)
@@ -49,32 +54,45 @@ while True:
       decimal_string = ""
       base58_string = ""
       print("All variables cleared")
+      print()
     case "d ":
       data_string = command[2:]
       print("Data set to  : " + data_string)
+      print()
     case "s ":
       secret_string = command[2:]
       data_string = "*"
       print("Secret set to: " + secret_string)
       print("Data set to  : " + data_string)
+      print()
     case "sc":
       secret_string = ""
       print("Secret cleared")
+      print()
     case "dc":
       data_string = ""
       print("Data cleared")
+      print()
     case "sd":
       secret_string = decimal_string
       data_string = "*"
+      print("Secret set to decimal string")
+      print()
     case "sh":
       secret_string = hash_string
       data_string = "*"
+      print("Secret set to hash")
+      print()
     case "sl":
       secret_string = base58_string[0:17]
       data_string = "*"
+      print("Secret set to left side of Base58")
+      print()
     case "sr":
       secret_string = base58_string[19:]
       data_string = "*"
+      print("Secret set to right side of Base 58")
+      print()
     case "q":
       del secret_string
       del data_string
@@ -84,6 +102,7 @@ while True:
       del base58_string
       del hash
       print("Sensitive data erased.")
+      print()
       break
     case "?":
       showHelp()
@@ -95,17 +114,12 @@ while True:
   hash.update(encoding_string.encode('utf-8'))
   hash_string = hash.hexdigest()
 
-  decimal_string = hash_string
-  decimal_string = decimal_string.replace("a", "0")
-  decimal_string = decimal_string.replace("b", "1")
-  decimal_string = decimal_string.replace("c", "2")
-  decimal_string = decimal_string.replace("d", "3")
-  decimal_string = decimal_string.replace("e", "4")
-  decimal_string = decimal_string.replace("f", "5")
-  decimal_string = decimal_string[0:4] + "-" + decimal_string[4:8] + "-" + decimal_string[8:12] + "-" + decimal_string[12:16] + "-" + decimal_string[16:20] + "-" + decimal_string[20:24] + "-" + decimal_string[24:28] + "-" + decimal_string[28:32] + "-" + decimal_string[32:36] + "-" + decimal_string[36:40]
+  decimal_string = "0x" + hash_string
+  decimal_string = literal_eval(decimal_string)
+  decimal_string = str(decimal_string)
+  decimal_string = decimal_string[0:5] + " " + decimal_string[5:10] + " " + decimal_string[10:15] + " " + decimal_string[15:20] + " " + decimal_string[20:25] + " " + decimal_string[25:30] + " " + decimal_string[30:35] + " " + decimal_string[35:40] + " " + decimal_string[40:45] + " " + decimal_string[45:50] + " " + decimal_string[50:55] + " " + decimal_string[55:60] + " " + decimal_string[60:65] + " " + decimal_string[65:70] + " " + decimal_string[70:75]
   
   base58_string = str(base58.b58encode(bytes.fromhex(hash_string)), encoding='utf-8')
   base58_string = base58_string[0:5] + "-" + base58_string[5:10] + "-" + base58_string[10:15] + "  " + base58_string[15:20] + "-" + base58_string[20:25] + "-" + base58_string[25:30]
   
   continue
-
